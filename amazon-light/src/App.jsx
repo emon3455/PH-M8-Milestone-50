9801586d37478cd8f720311d9c2a395be7850cd7
Emon <emon.mhk69@gmail.com>
2023-03-27 03:18:18 +0600

    module done ready to deploy

--- amazon-light/src/App.jsx
+++ amazon-light/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
 import Shop from './Components/Shop/Shop'
-
 function App() {
   const [count, setCount] = useState(0)
 
