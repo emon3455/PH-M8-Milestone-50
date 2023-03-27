f25c5e42f1602224c1ac236258e6b75cf8599a07
Emon <emon.mhk69@gmail.com>
2023-03-28 00:43:37 +0600

    saved cart to local storage

--- amazon-light/src/Components/Shop/Shop.jsx
+++ amazon-light/src/Components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../../public/utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import "./Shop.css"
@@ -24,7 +25,13 @@ const Shop = () => {
     const handleAddToCart = (product)=>{
         const newCart = [...cart , product];
         setCart(newCart);
+        addToDb(product.id)
     }
+
+    useEffect(()=>{
+        const storedCart = getShoppingCart();
+        console.log(storedCart);
+    },[])
 
 
     return (
